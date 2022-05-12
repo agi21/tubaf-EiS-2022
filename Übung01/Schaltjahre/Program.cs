57157689b7bb67de27a728eae2b23c29940116f2
agi21 <anne.gierich@gmx.de>
2022-05-12 11:22:41 +0200

    Added Schaltjahr

---
+++ Übung01/Schaltjahre/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        uint year;
+        string yearString = "";
+
+        //Endlosschleife 
+        //fußgesteurte do-while Schleife
+        do
+        {
+            Console.Write("Year -> ");
+            yearString = Console.ReadLine();
+
+            //Abbruchbedingung
+            if(yearString.Length == 0)
+            {
+                Console.WriteLine("The end!");
+                return;
+            }
+
+            //sonst in uint parsen
+            try
+            {
+                year = uint.Parse(yearString);
+            }
+            catch
+            {
+                Console.WriteLine("Bad format");
+                continue;
+            }
+
+            //"Berechnung" durchführen
+            bool isLeapYear = (year % 4 == 0) 
+                            && (year % 100 != 0) 
+                            || (year % 400 == 0);
+
+            Console.WriteLine($"The year {year} is a leap year: {isLeapYear}");
+
+        }while (true);
+    }
+}
+
