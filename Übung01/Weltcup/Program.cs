abf92d8564740d437b8114e6fddd1acc2ff2c70d
agi21 <anne.gierich@gmx.de>
2022-05-17 22:11:36 +0200

    added Task7

---
+++ Übung01/Weltcup/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+            string[] platzierung = new string[] {"Marte Olsbu Röiseland", "Elvira Öberg", "Lisa Theresa Hauser", "Hanna Öberg", "Anais Chevalier-Bouchet",
+                                                    "Denise Herrmann", "Dzinara Alimbekava", "Justine Braisaz-Bouchet", "Dorothea Wierer", "Marketa Davidova"};
+            Console.WriteLine("Platzierung | Name");
+            Console.WriteLine("------------|------------------------------");
+
+    
+            for(int j = 0; j< platzierung.Length; j++)
+            {
+                Console.WriteLine("{0,-12}|{1,-30}", j+1, platzierung[j]);
+            }
+
+            int i; 
+            try{
+                Console.Write("\nEingabe Platz: ");
+                i = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Auf Paltz {i} ist {platzierung[i-1]}\n");
+            }catch{
+                Console.WriteLine("Falscher Input");
+                return;
+            }
+    }
+}
+
